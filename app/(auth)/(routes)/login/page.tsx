fc6b8f791c4df60934c680e8b1d132ae5ae34d48
aswin <aswinkarat@gmail.com>
2023-12-13 12:09:37 +0530

    first commit

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+  return <div>LoginPage(UnProtected)</div>;
+};
+
+export default LoginPage;
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,5 @@
+const RootPage = () => {
+  return <div>RootPage(Protected)</div>;
+};
+
+export default RootPage;
