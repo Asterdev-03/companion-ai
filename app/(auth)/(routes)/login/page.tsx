75e3e5600b0bfdd87fffaf2df9c417efa8bb1307
aswin <aswinkarat@gmail.com>
2023-12-13 12:48:37 +0530

    Feat: Clerk Authentication

--- app/(auth)/(routes)/login/page.tsx
+++
@@ -1,5 +0,0 @@
-const LoginPage = () => {
-  return <div>LoginPage(UnProtected)</div>;
-};
-
-export default LoginPage;
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,5 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 const RootPage = () => {
-  return <div>RootPage(Protected)</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default RootPage;
