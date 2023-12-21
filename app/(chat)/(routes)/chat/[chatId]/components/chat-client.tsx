0c0419b95307336d087b9368b0f9abc4a906a20e
aswin <aswinkarat@gmail.com>
2023-12-21 16:46:11 +0530

    Feat: Added Companion Chat UI

--- app/(chat)/(routes)/chat/[chatId]/components/chat-client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/chat-client.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-import { ChatHeader } from "@/components/chat-header";
 import { Companion, Message } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+import { useCompletion } from "ai/react";
+
+import { ChatHeader } from "@/components/chat-header";
+import { CharForm } from "@/components/chat-form";
 
 interface ChatClientProps {
   companion: Companion & {
@@ -13,9 +18,45 @@ interface ChatClientProps {
 }
 
 export const ChatClient = ({ companion }: ChatClientProps) => {
+  const router = useRouter();
+
+  const [messages, setMessages] = useState<any[]>(companion.messages);
+
+  const { input, isLoading, handleInputChange, handleSubmit, setInput } =
+    useCompletion({
+      api: `/api/chat/${companion.id}`,
+      onFinish(prompt, completion) {
+        const systemMessage = {
+          role: "system",
+          content: completion,
+        };
+        setMessages((current) => [...current, systemMessage]);
+        setInput("");
+
+        router.refresh();
+      },
+    });
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    const userMessage = {
+      role: "system",
+      content: input,
+    };
+    setMessages((current) => [...current, userMessage]);
+
+    handleSubmit(event);
+  };
+
   return (
     <div className="flex flex-col h-full p-4 space-y-2">
       <ChatHeader companion={companion} />
+      <div>Messages TODO</div>
+      <CharForm
+        isLoading={isLoading}
+        input={input}
+        handleInputChange={handleInputChange}
+        onSubmit={onSubmit}
+      />
     </div>
   );
 };
