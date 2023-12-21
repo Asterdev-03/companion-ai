82183276517f56c9c7a13df6b77be4ae419c4bcb
aswin <aswinkarat@gmail.com>
2023-12-21 13:55:21 +0530

    Feat: Add Companion List display menu

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,15 +1,44 @@
 import { Categories } from "@/components/categories";
+import { Companions } from "@/components/companions";
 import { SearchInput } from "@/components/search-input";
 import prismadb from "@/lib/prismadb";
 
-const RootPage = async () => {
+// searchParams is a convention in nextjs server component
+interface RootPageProps {
+  searchParams: {
+    categoryId: string;
+    name: string;
+  };
+}
+
+const RootPage = async ({ searchParams }: RootPageProps) => {
   // fetch all the categories present in prisma db
   const categories = await prismadb.category.findMany();
+
+  const data = await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
 
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
       <Categories data={categories} />
+      <Companions data={data} />
     </div>
   );
 };
